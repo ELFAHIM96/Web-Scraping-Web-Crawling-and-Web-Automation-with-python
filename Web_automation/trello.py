a0853c7cec03d563e4079f7be1ba5d021ed0eab1
ELFAHIM96 <elfahimomar95@gmail.com>
2023-05-11 13:48:32 +0200

    web_automation

---
+++ Web_automation/trello.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time 
+import json
+from datetime import date
+
+import os
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+
+    main()
+    print("hi")
